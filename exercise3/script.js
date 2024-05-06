f0b5080bb44c03016d3a906c4d0e1da45248758c
Tasha <nataliiamelnykova9@gmail.com>
2024-05-06 08:50:33 +0200

    upd

--- exercise3/script.js
+++ exercise3/script.js
@@ -1,8 +1,30 @@
-let hours;
-const now = new Date();
+// let hours;
+// const now = new Date();
 // console.log(now);
-hours = 80000;
-const someWhen = now.getTime() + (hours * 60 * 60 * 1000);
-console.log(someWhen);
-const exactTime = new Date(someWhen.toString());
-console.log(exactTime);
+// hours = 80000;
+// const someWhen = now.getTime() + (hours * 60 * 60 * 1000);
+// console.log(someWhen);
+// const exactTime = new Date(someWhen);
+// console.log(exactTime);
+
+const hoursInput = document.querySelector("input");
+const now = new Date();
+const today = document.querySelector(".today");
+const inNHours = document.querySelector(".in-n-hours");
+
+hoursInput.addEventListener("change", dates);
+
+function dates() {
+    const hours = hoursInput.value;
+    const someWhen = now.getTime() + (hours * 60 * 60 * 1000);
+    const exactTime = new Date(someWhen);
+    today.textContent = `Today is: ${now}`;
+    if (hours) {
+        inNHours.textContent = `In ${hours} hours it will be: ${exactTime}`;
+    }
+    
+}
+
+
+dates();
+
